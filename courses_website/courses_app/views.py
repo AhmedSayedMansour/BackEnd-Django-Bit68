dd67e2e2adb56b4e6b608f261ecf020f47286b72
Ahmed Sayed Mansour <ahmed.mans20719@gmail.com>
2022-02-10 08:30:51 +0200

    Added remaining views

--- courses_website/courses_app/views.py
+++ courses_website/courses_app/views.py
@@ -10,6 +10,54 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly    #not Auth bu
 from rest_framework.permissions import IsAuthenticated              #not Auth can't read
 
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    #permission_classes = ---
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """Handles reading courses"""
+
+    serializer_class = serializers.CourseSerializer
+    queryset = models.Course.objects.all()
+    #authentication_classes = ---
+    #permission_classes = ---
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'category_id', 'price', 'updated_at', 'language',)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """Handles reading categories"""
+
+    serializer_class = serializers.CategorySerializer
+    queryset = models.Category.objects.all()
+    #authentication_classes = ---
+    #permission_classes = ---
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """Handles reading articles"""
+
+    serializer_class = serializers.ArticleSerializer
+    queryset = models.Article.objects.all()
+    #authentication_classes = ---
+    #permission_classes = ---
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title',)
+
+
 class PartnerViewSet(viewsets.ModelViewSet):
     """Handles reading Partners """
     serializer_class = serializers.PartnerSerializer
@@ -17,4 +65,4 @@ class PartnerViewSet(viewsets.ModelViewSet):
     #authentication_classes = ---
     #permission_classes = ---
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('name')
+    search_fields = ('name',)
